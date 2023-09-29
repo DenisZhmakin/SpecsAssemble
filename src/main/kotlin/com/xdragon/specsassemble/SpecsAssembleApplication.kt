3fc5742201c982d59a275e9b0f4c5627becfbcfb
Denis Zhmakin <den.zhmakin.00@gmail.com>
2023-09-29 21:39:35 +0300

    Added a project template

---
+++ src/main/kotlin/com/xdragon/specsassemble/SpecsAssembleApplication.kt
@@ -0,0 +1,11 @@
+package com.xdragon.specsassemble
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpecsAssembleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpecsAssembleApplication>(*args)
+}
